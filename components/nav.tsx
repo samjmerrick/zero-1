2d4d1fad4820722a85c18fe0e26c719e152b20bb
sammerrick <samjmerrick@gmail.com>
2022-07-01 10:14:54 +0100

    Add logo and nav elements

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,30 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../components/logo'
+
+
 
 function Nav() {
-    return
+    return (
+        <nav className="fixed flex flex-wrap w-screen top-0 z-50 h-50 justify-between bg-slate-900 p-6">
+            <a href='#'>
+                <Logo />
+            </a>
+
+            <div className="text-sm space-x-4 ">
+                <a href="#about" className="block lg:inline-block">
+                    About
+                </a>
+                <a href="#projects" className="block lg:inline-block">
+                    Projects
+                </a>
+                <a href="#contact" className="block lg:inline-block">
+                    Contact
+                </a>
+            </div>
+
+        </nav>
+    )
 }
 
 export default Nav
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,46 @@
-import Link from 'next/link'
+
+
+import Nav from '../components/nav'
+
 
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World.
-    </h1>
+    <div className="bg-slate-900 text-white ">
+      <Nav />
+      <section className='flex flex-col h-screen'>
+        <div className=' flex flex-grow items-center'>
+          <div className='px-10'>
+            <h1 className=" grow">
+              Digital products from Zero to One.
+            </h1>
+            <p>
+              We create mobile apps and websites from scratch. With a range of product management, design and development expertise
+            </p>
+
+
+          </div>
+
+
+        </div>
+
+        <div className='absolute right-0 bottom-0 p-10'>
+          <a href='#about'>
+            <p>
+              ↓ Scroll to learn more
+            </p>
+          </a>
+        </div>
+
+
+
+      </section>
+      <section id="about" className='h-screen flex items-center'>
+        <h1 className=" grow">
+          and other stuff
+        </h1>
+      </section>
+
+    </div>
   )
 }
