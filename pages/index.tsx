1b8ead9f962b331d40316881ecbe77ff45bfaf7e
sammerrick <samjmerrick@gmail.com>
2022-07-01 13:19:38 +0100

    Add typewriter effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 
 
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
 import Nav from '../components/nav'
+import Typewriter from 'typewriter-effect';
 
 
 
@@ -8,49 +10,61 @@ export default function Home() {
   return (
     <div className="bg-slate-900 text-white ">
       <Nav />
-      <section id='home' className='flex flex-col h-screen'>
-        <div className=' flex flex-grow items-center'>
+      <ParallaxProvider>
+        <section id='home' className='h-screen flex items-center'>
+          <div className='w-full h-1/2  absolute bottom-0'>
+            <div className='bg-slate-800 skew-y-12 h-3/4' />
+          </div>
           <div className='px-10'>
-            <h1 className=" grow">
-              Digital products from Zero to One.
-            </h1>
-            <p>
-              We create mobile apps and websites from scratch. With a range of product management, design and development expertise
-            </p>
+            <Parallax speed={10}>
+              <h1 className=" grow">
+                Digital products from Zero to One.
+              </h1>
+            </Parallax>
+            <Parallax speed={0}>
+              <div className='text-3xl flex flex-row'>
+                We create&nbsp;
 
-
+                <Typewriter
+                  options={{
+                    strings: ['mobile apps', 'responsive websites', 'shopify stores', 'personal portfolios'],
+                    loop: true,
+                    autoStart: true,
+                    delay: 20,
+                    deleteSpeed: 20,
+                  }}
+                />
+              </div>
+            </Parallax>
           </div>
 
+          <div className='absolute right-0 bottom-0 p-10'>
+            <a href='#about'>
+              <p>
+                ↓ Scroll to learn more
+              </p>
+            </a>
+          </div>
+        </section>
 
-        </div>
+        <section id='about' className='h-screen flex items-center'>
+          <h1 className="grow">
+            About
+          </h1>
+        </section>
 
-        <div className='absolute right-0 bottom-0 p-10'>
-          <a href='#about'>
-            <p>
-              ↓ Scroll to learn more
-            </p>
-          </a>
-        </div>
+        <section id='projects' className='h-screen flex items-center'>
+          <h1 className="grow">
+            Projects
+          </h1>
+        </section>
 
-      </section>
-      <section id='about' className='h-screen flex items-center'>
-        <h1 className="grow">
-          About
-        </h1>
-      </section>
-
-      <section id='projects' className='h-screen flex items-center'>
-        <h1 className="grow">
-          Projects
-        </h1>
-      </section>
-
-      <section id='contact' className='h-screen flex items-center'>
-        <h1 className="grow">
-          Contact
-        </h1>
-      </section>
-
+        <section id='contact' className='h-screen flex items-center'>
+          <h1 className="grow">
+            Contact
+          </h1>
+        </section>
+      </ParallaxProvider>
     </div>
   )
 }
