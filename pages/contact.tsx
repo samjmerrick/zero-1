40a03e32ebcc80ae31b79d732a5d5eac83d92cd5
Sam Merrick <samjmerrick@gmail.com>
2022-07-06 08:24:46 +0100

    Hooked contact form to Google Sheets

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,37 @@
 import { Page, Content } from "../components/page"
 
+
+
+if (typeof window !== "undefined") {
+    window.addEventListener("load", function () {
+        const form = document.getElementById('contact-form') as HTMLFormElement;
+        const submit = document.getElementById('Submit') as HTMLButtonElement;
+
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            const data = new FormData(form);
+            const target = e.target as HTMLFormElement;
+            const action = target.action;
+
+            submit.disabled = true;
+            submit.innerText = 'Sending...';
+
+            fetch(action, {
+                method: 'POST',
+                body: data,
+                mode: 'no-cors'
+            })
+                .then(() => {
+                    alert("Thanks, we'll get back to you soon!");
+                    form.reset();
+                    submit.disabled = false;
+                    submit.innerText = 'Send';
+                })
+        });
+    });
+}
+
 function Contact() {
     return (
         <Page id='contact'>
@@ -11,17 +43,18 @@ function Contact() {
                     Interested in working with us on the next big product? Fill out this form and we'll get back to you in 1-2 business days
                 </p>
                 <form
-                    action="/api/form"
+                    action="https://script.google.com/macros/s/AKfycbzeqJfpQc3ckStHuiF2bOGZJxEj8QC0z_k56HmUn3RvxKxVNH0-ncpOFQPIMvrHIHYMYQ/exec"
                     method="post"
                     className="flex flex-col max-w-xl"
+                    id="contact-form"
                 >
                     <label htmlFor="name">
                         Your Name
                     </label>
                     <input
                         type="text"
-                        id="name"
-                        name="name"
+                        id="Name"
+                        name="Name"
                         placeholder="Barry Allen"
                         required
                         className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
@@ -31,8 +64,8 @@ function Contact() {
                     </label>
                     <input
                         type="email"
-                        id="email"
-                        name="email"
+                        id="Email"
+                        name="Email"
                         placeholder="name@example.com"
                         required
                         className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
@@ -41,18 +74,19 @@ function Contact() {
                         Your message
                     </label>
                     <textarea
-                        id="message"
-                        name="message"
+                        id="Message"
+                        name="Message"
                         placeholder="My exciting product idea.."
                         rows={4}
                         required
                         className="p-3 mb-5  mt-1 bg-slate-800 rounded-lg resize-none"
                     />
-
                     <button
+                        id="Submit"
                         type="submit"
                         className="p-3 bg-slate-700 max-w-xs rounded-lg justify-center"
                     >
+
                         Send
                     </button>
                 </form>
