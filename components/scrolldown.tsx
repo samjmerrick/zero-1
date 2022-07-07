ea10a131ce3164b8a4bf54b81dc56a44b8e440a5
Sam Merrick <samjmerrick@gmail.com>
2022-07-07 17:48:16 +0100

    Always show scrolldown

--- components/scrolldown.tsx
+++ components/scrolldown.tsx
@@ -3,7 +3,7 @@ function ScrollDown(props: {
     label: string,
     id: string,
 }) {
-    return <div className='hidden lg:inline absolute right-0 bottom-0 p-10'>
+    return <div className='absolute right-0 bottom-0 p-10'>
         <a href={props.id}>
             <p>
                 ↓ {props.label}
