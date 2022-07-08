718895eed5323addbbccf29b38cd0807438e1983
Sam Merrick <samjmerrick@gmail.com>
2022-07-08 15:50:44 +0100

    Added particle effects to hero

--- pages/hero.tsx
+++ pages/hero.tsx
@@ -1,11 +1,31 @@
+// @ts-nocheck
+
 import { Parallax } from "react-scroll-parallax"
 import { PageFull, Content } from "../components/page"
 import Typewriter from 'typewriter-effect';
 import ScrollDown from "../components/scrolldown";
 
+import Particles, { IParticlesProps } from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { Engine } from "tsparticles-engine";
+import React, { useCallback } from "react";
+
+import Options from "../components/particles.json"
+
 function Hero() {
+    // this customizes the component tsParticles installation
+    const customInit = useCallback(async (engine: Engine) => {
+        // this adds the bundle to tsParticles
+        await loadFull(engine);
+    }, []);
+
     return (
         <PageFull id='home'>
+            <Particles
+                options={Options}
+                init={customInit}
+                className='h-full'
+            />;
             <Content>
                 <Parallax speed={10}>
                     <h1 className=" grow">
@@ -13,7 +33,7 @@ function Hero() {
                     </h1>
                 </Parallax>
                 <Parallax speed={0}>
-                    <div className='text-3xl inline'>
+                    <div className='text-4xl inline'>
                         <Typewriter
                             options={{
                                 strings: ['We build mobile apps', 'We design responsive websites', 'We create and manage ecommerce stores', 'We conduct user research', 'We shape the future'],
