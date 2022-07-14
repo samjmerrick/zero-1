83342824b678293adfbd0f4ef8f98fbde3948b2e
Sam Merrick <samjmerrick@gmail.com>
2022-07-14 20:50:47 +0100

    Improve project scroll

--- components/page.tsx
+++ components/page.tsx
@@ -1,6 +1,6 @@
 function Content({ children }) {
     return (
-        <div className='relative py-5 px-5 md:px-10 mt-20 w-full flex flex-col space-y-8'>
+        <div className='relative py-5 px-5 md:px-10 mt-20 w-full flex flex-col space-y-8 '>
             {children}
         </div>
     )
@@ -8,7 +8,7 @@ function Content({ children }) {
 
 function Page({ children, id }) {
     return (
-        <section id={id} className='w-screen flex'>
+        <section id={id} className='w-full '>
             {children}
         </section>
     )
@@ -16,7 +16,7 @@ function Page({ children, id }) {
 
 function PageFull({ children, id }) {
     return (
-        <section id={id} className='w-screen h-screen flex items-center '>
+        <section id={id} className='w-full h-screen flex items-center '>
             {children}
         </section>
     )
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,6 @@
+import Icon from "@mdi/react";
 import { Page, Content } from "../components/page"
+import { mdiArrowLeft, mdiArrowRight } from '@mdi/js';
 
 function Project(props: {
     title: string,
@@ -14,7 +16,7 @@ function Project(props: {
 
     return (
         <div
-            className=' bg-slate-800 rounded-lg p-10 max-w-md min-w-[90%] sm:min-w-[66%] md:min-w-[50%] lg:min-w-[25%] cursor-pointer '
+            className=' bg-slate-800 rounded-lg p-10 max-w-md min-w-[90%] sm:min-w-[66%] md:min-w-[50%] lg:min-w-[40%] xl:min-w-[25%] cursor-pointer '
             onClick={() => openUrl(props.url)}
         >
             <img
@@ -29,40 +31,64 @@ function Project(props: {
 }
 
 function Projects() {
+
+    const distance = 200;
+
+    function scrollLeft() {
+        document.getElementById('carousel').scrollBy({
+            left: -distance,
+            behavior: 'smooth'
+        });
+    }
+
+    function scrollRight() {
+        document.getElementById('carousel').scrollBy({
+            left: distance,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <Page id='projects'>
             <Content>
                 <h2 className="text-center">
                     Showcase
                 </h2>
-
-                <div className=" flex flex-row space-x-5 md:space-x-10 no-scrollbar">
-
-                    <Project
-                        title='Tend'
-                        caption='Tend is a mindfulness and meditation app aimed at a Gen-Z audience'
-                        imagePath='/Tend.png'
-                        url='https://www.thetendapp.com/'
-                    />
-                    <Project
-                        title='HandDeco'
-                        caption='HandDeco is a handmade and custom embroidery start-up operating in Switzerland.'
-                        imagePath='/HandDeco.jpeg'
-                        url='https://www.handdeco.com/'
-                    />
-                    <Project
-                        title='Virgin Red'
-                        caption='Virgin Red is a loyalty and rewards offering for customers of the Virgin Group.'
-                        imagePath='/VirginRed.png'
-                        url='https://www.virgin.com/virgin-red'
-                    />
-                    <Project
-                        title='Student Beans'
-                        caption='Student Beans is a market leading student discount and rewards platform.'
-                        imagePath='/StudentBeans.png'
-                        url='https://www.studentbeans.com/uk'
-                    />
+                <div className="flex flex-row space-x-5">
+                    <button onClick={scrollLeft} className="hidden lg:inline">
+                        <Icon path={mdiArrowLeft} size={2.5} />
+                    </button>
+                    <span className="flex flex-row space-x-5 md:space-x-10 no-scrollbar overflow-auto" id="carousel">
+                        <Project
+                            title='Tend'
+                            caption='Tend is a mindfulness and meditation app aimed at a Gen-Z audience'
+                            imagePath='/Tend.png'
+                            url='https://www.thetendapp.com/'
+                        />
+                        <Project
+                            title='HandDeco'
+                            caption='HandDeco is a handmade and custom embroidery start-up operating in Switzerland.'
+                            imagePath='/HandDeco.jpeg'
+                            url='https://www.handdeco.com/'
+                        />
+                        <Project
+                            title='Virgin Red'
+                            caption='Virgin Red is a loyalty and rewards offering for customers of the Virgin Group.'
+                            imagePath='/VirginRed.png'
+                            url='https://www.virgin.com/virgin-red'
+                        />
+                        <Project
+                            title='Student Beans'
+                            caption='Student Beans is a market leading student discount and rewards platform.'
+                            imagePath='/StudentBeans.png'
+                            url='https://www.studentbeans.com/uk'
+                        />
+                    </span>
+                    <button onClick={scrollRight} className="hidden lg:inline">
+                        <Icon path={mdiArrowRight} size={2.5} />
+                    </button>
                 </div>
+
             </Content>
         </Page>
     )
