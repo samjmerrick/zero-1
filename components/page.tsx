0e234961b313b1f05372a4071bcdf754cb529bb3
Sam Merrick <samjmerrick@gmail.com>
2022-07-28 19:14:56 +0100

    Added UX sub-page in mdx

--- components/page.tsx
+++ components/page.tsx
@@ -1,15 +1,27 @@
+import Head from 'next/head'
 import Nav from './nav'
 
-function Page({ children, id }) {
+function Page({ children, meta }) {
     return (
-        <div className="bg-slate-900 text-white">
+        <>
+            <Head>
+                <title>{meta.title} | Zero 1 Studio</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <Nav />
-            <Section id={id}>
-                <Content>
+            <div className="bg-slate-900 text-white w-full h-screen">
+
+                <div className='container w-full md:max-w-4xl mx-auto pt-20 space-y-4 px-5'>
                     {children}
-                </Content>
-            </Section>
-        </div>
+                </div>
+            </div>
+        </>
     )
 }
 
