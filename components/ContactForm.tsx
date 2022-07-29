f169d4242004cd707b0890592b51aeebc531ed31
Sam Merrick <samjmerrick@gmail.com>
2022-07-29 18:49:37 +0100

    Smaller contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -30,53 +30,54 @@ if (typeof window !== "undefined") {
 }
 
 function ContactForm() {
-    return (<form
-        action="https://script.google.com/macros/s/AKfycbzeqJfpQc3ckStHuiF2bOGZJxEj8QC0z_k56HmUn3RvxKxVNH0-ncpOFQPIMvrHIHYMYQ/exec"
-        method="post"
-        className="flex flex-col w-full"
-        id="contact-form"
-    >
-        <label htmlFor="name">
-            Your Name
-        </label>
-        <input
-            type="text"
-            id="Name"
-            name="Name"
-            placeholder="Barry Allen"
-            required
-            className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
-        />
-        <label htmlFor="email">
-            Your email address
-        </label>
-        <input
-            type="email"
-            id="Email"
-            name="Email"
-            placeholder="name@example.com"
-            required
-            className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
-        />
-        <label htmlFor="message">
-            Your message
-        </label>
-        <textarea
-            id="Message"
-            name="Message"
-            placeholder="My exciting product idea.."
-            rows={4}
-            required
-            className="p-3 mb-5  mt-1 bg-slate-800 rounded-lg resize-none"
-        />
-        <button
-            id="Submit"
-            type="submit"
-            className="p-3 bg-slate-700 w-full max-w-xs rounded-lg justify-center self-center"
+    return (
+        <form
+            action="https://script.google.com/macros/s/AKfycbzeqJfpQc3ckStHuiF2bOGZJxEj8QC0z_k56HmUn3RvxKxVNH0-ncpOFQPIMvrHIHYMYQ/exec"
+            method="post"
+            className="flex flex-col w-full max-w-2xl"
+            id="contact-form"
         >
-            Send
-        </button>
-    </form>
+            <label htmlFor="name">
+                Your Name
+            </label>
+            <input
+                type="text"
+                id="Name"
+                name="Name"
+                placeholder="Barry Allen"
+                required
+                className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
+            />
+            <label htmlFor="email">
+                Your email address
+            </label>
+            <input
+                type="email"
+                id="Email"
+                name="Email"
+                placeholder="name@example.com"
+                required
+                className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
+            />
+            <label htmlFor="message">
+                Your message
+            </label>
+            <textarea
+                id="Message"
+                name="Message"
+                placeholder="My exciting product idea.."
+                rows={4}
+                required
+                className="p-3 mb-5  mt-1 bg-slate-800 rounded-lg resize-none"
+            />
+            <button
+                id="Submit"
+                type="submit"
+                className="p-3 bg-slate-700 w-full max-w-xs rounded-lg justify-center self-center"
+            >
+                Send
+            </button>
+        </form>
 
     )
 }
--- sections/contact.tsx
+++ sections/contact.tsx
@@ -7,7 +7,10 @@ function Contact() {
             <p className="text-center">Want to work with us on the next exciting product? Fill in the details or email at
                 <a className='font-bold text-green-600 ' href='mailto:hello@zero-1.studio'> hello@zero-1.studio</a>
             </p>
-            <ContactForm />
+            <div className='flex justify-center'>
+                <ContactForm />
+            </div>
+
         </Section>
     )
 }
