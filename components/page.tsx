046da790efcae341243ef9291d6a005a21b06f5b
Sam Merrick <samjmerrick@gmail.com>
2022-07-30 16:05:46 +0100

    String concat title

--- components/page.tsx
+++ components/page.tsx
@@ -5,7 +5,7 @@ function Page({ children, meta }) {
     return (
         <>
             <Head>
-                <title>{meta.title} | Zero 1 Studio</title>
+                <title>{meta.title + ' | Zero 1 Studio'}</title>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
