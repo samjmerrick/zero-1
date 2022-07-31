4fdd56e0b4bf67cfc55d19091afa7bd9df9cb927
Sam Merrick <samjmerrick@gmail.com>
2022-07-31 10:17:38 +0100

    Added email icon to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,24 @@
+import Icon from "@mdi/react";
+import { mdiEmailOutline } from '@mdi/js';
 import Container from "./Container";
 
 function Footer() {
     return (
-        <footer className="py-10 bg-slate-800 ">
+        <footer className="py-10 bg-slate-900 ">
             <Container>
-                <div className="flex flex-row justify-between items-center">
-                    <a className=' text-3xl text-green-500 ' href='mailto:hello@zero-1.studio'>
-                        hello@zero-1.studio
-                    </a>
+                <div className="flex justify-between items-center">
+                    <p className="flex flex-row items-center space-x-3">
+                        <Icon
+                            path={mdiEmailOutline}
+                            color='white'
+                            size={1.5}
+                        />
+
+                        <a className=' text-green-600 font-bold ' href='mailto:hello@zero-1.studio'>
+                            hello@zero-1.studio
+                        </a>
+                    </p>
+
                     <p className="text-white">
                         © Zero 1 Studio 2022
                     </p>
