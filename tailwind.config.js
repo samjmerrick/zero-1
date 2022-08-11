2526afdc3ed4f246b4f58a4897f738e588cb5ebf
Sam Merrick <samjmerrick@gmail.com>
2022-08-11 19:58:46 +0100

    Added tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./sections/**/*.{js,ts,jsx,tsx}",
+    './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('tw-elements/dist/plugin'),
   ],
 }
