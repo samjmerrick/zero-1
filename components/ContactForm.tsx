aebf3728e00698c1267cf8420a0350f1e686094f
Sam Merrick <samjmerrick@gmail.com>
2022-08-21 17:03:37 +0100

    Added cal.com calendar

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,85 +1,28 @@
-
-if (typeof window !== "undefined") {
-    window.addEventListener("load", function () {
-        const form = document.getElementById('contact-form') as HTMLFormElement;
-        const submit = document.getElementById('Submit') as HTMLButtonElement;
-
-        form.addEventListener("submit", function (e) {
-            e.preventDefault();
-
-            const data = new FormData(form);
-            const target = e.target as HTMLFormElement;
-            const action = target.action;
-
-            submit.disabled = true;
-            submit.innerText = 'Sending...';
-
-            fetch(action, {
-                method: 'POST',
-                body: data,
-                mode: 'no-cors'
-            })
-                .then(() => {
-                    alert("Thanks, we'll get back to you soon!");
-                    form.reset();
-                    submit.disabled = false;
-                    submit.innerText = 'Send';
-                })
-        });
-    });
-}
+import { useEffect } from "react";
+import Cal, { getCalApi } from "@calcom/embed-react";
 
 function ContactForm() {
+
+    useEffect(() => {
+        (async function () {
+            const cal = await getCalApi();
+            cal("ui", { "styles": { "branding": { "brandColor": "#1e293b" } } });
+        })();
+    }, [])
+
     return (
         <form
             action="https://script.google.com/macros/s/AKfycbzeqJfpQc3ckStHuiF2bOGZJxEj8QC0z_k56HmUn3RvxKxVNH0-ncpOFQPIMvrHIHYMYQ/exec"
             method="post"
-            className="flex flex-col w-full max-w-2xl space-y-4"
+            className="flex flex-col w-full space-y-4"
             id="contact-form"
         >
             <div className="text-center">
-                <h2>Get in touch</h2>
+                <h2>Book a free consultation</h2>
+                <p>We love to talk about ideas, our first call is completely free!</p>
             </div>
-            <label htmlFor="name">
-                Your Name
-            </label>
-            <input
-                type="text"
-                id="Name"
-                name="Name"
-                placeholder="e.g. Barry Allen"
-                required
-                className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
-            />
-            <label htmlFor="email">
-                Your email address
-            </label>
-            <input
-                type="email"
-                id="Email"
-                name="Email"
-                placeholder="name@example.com"
-                required
-                className="p-3 mb-5 mt-1 bg-slate-800 rounded-lg"
-            />
-            <label htmlFor="message">
-                Your message
-            </label>
-            <textarea
-                id="Message"
-                name="Message"
-                placeholder="Message here please!"
-                rows={4}
-                required
-                className="p-3 mb-5  mt-1 bg-slate-800 rounded-lg resize-none"
-            />
-            <button
-                id="Submit"
-                type="submit"
-                className="p-3 bg-slate-700 w-full max-w-xs rounded-lg justify-center self-center"
-            >
-                Send
-            </button>
+            <Cal calLink="sam-merrick/30min" style={{ width: "100%", height: "100%", overflow: "hidden" }} />
+
         </form>
 
     )
