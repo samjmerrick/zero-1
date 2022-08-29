d62327698895c180e427ef168eb5c2aa5ced0240
Sam Merrick <samjmerrick@gmail.com>
2022-08-29 09:55:09 +0100

    Fixed services pages

--- next.config.js
+++ next.config.js
@@ -10,12 +10,9 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
     // Append the default value with md extensions
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
-
-module.exports = {
     experimental: {
         images: {
             allowFutureImage: true,
         },
     },
-}
+})
