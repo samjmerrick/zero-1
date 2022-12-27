590f8a045b9c3074e79d9d913c15c5ba0b2c3502
Sam Merrick <samjmerrick@gmail.com>
2022-12-27 17:04:43 +0000

    Copy changes

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,33 +1,31 @@
-
-import { mdiRocketLaunch, mdiHumanGreeting, mdiFinance } from '@mdi/js'
-import IconCard from '../IconCard'
-import ColumnLayout from '../ColumnLayout'
-import Section from '../Section'
+import { mdiRocketLaunch, mdiHumanGreeting, mdiFinance } from "@mdi/js";
+import IconCard from "../IconCard";
+import ColumnLayout from "../ColumnLayout";
+import Section from "../Section";
 
 function About() {
-    return (
-        <Section id='About'>
-            <h2>What makes us different?</h2>
-            <ColumnLayout>
-                <IconCard
-                    icon={mdiRocketLaunch}
-                    title='Startup Focused'
-                    description="Our focus on startups gives us deep experience on how best to solve their problems."
-                />
-                <IconCard
-                    icon={mdiHumanGreeting}
-                    title='User Centered'
-                    description="We keep users at the heart of our products, testing rapidly and frequently with real people."
-
-                />
-                <IconCard
-                    icon={mdiFinance}
-                    title='Outcome Oriented'
-                    description="We deliver more than just a product. We collaborate, define and deliver commercial results."
-                />
-            </ColumnLayout>
-        </Section>
-    )
+  return (
+    <Section id="About">
+      <h2>Why choose us?</h2>
+      <ColumnLayout>
+        <IconCard
+          icon={mdiRocketLaunch}
+          title="Startup Focused"
+          description="Our focus on startups gives us deep experience on how best to solve their problems."
+        />
+        <IconCard
+          icon={mdiHumanGreeting}
+          title="User Centered"
+          description="We keep users at the heart of our products, testing rapidly and frequently with real people."
+        />
+        <IconCard
+          icon={mdiFinance}
+          title="Outcome Oriented"
+          description="We deliver more than just a product. We collaborate, define and deliver commercial results."
+        />
+      </ColumnLayout>
+    </Section>
+  );
 }
 
-export default About
+export default About;
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import ColumnLayout from '../ColumnLayout';
+import ColumnLayout from "../ColumnLayout";
 
 function Hero() {
-    return (
-        <ColumnLayout>
-            <div className='flex flex-col space-y-8 pt-20 lg:w-3/5'>
-                <h1 className="text-6xl md:text-6xl ">
-                    Digital products from Zero to One
-                </h1>
-                <p className='text-2xl' >
-                    We are a team of creative technologists that build early stage digital products.
-                </p>
-            </div>
+  return (
+    <ColumnLayout>
+      <div className="flex flex-col space-y-8 pt-20">
+        <h1 className="text-6xl md:text-6xl ">
+          Digital products from Zero to One
+        </h1>
+        <p className="text-2xl">
+          We help startups and scale-ups to build innovative digital products.
+        </p>
+      </div>
 
-            <img src={'/illustrations/hero.svg'} className='lg:w-2/5' />
-        </ColumnLayout>
-    )
+      <img
+        src={
+          "https://gojilabs.com/wp-content/uploads/2022/12/27_May_06-copy-1-1621x1370.png"
+        }
+        className=""
+      />
+    </ColumnLayout>
+  );
 }
 
-export default Hero
+export default Hero;
