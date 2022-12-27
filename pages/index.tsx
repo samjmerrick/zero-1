84519df4025664cb12493d7f069d264e9c1186c8
Sam Merrick <samjmerrick@gmail.com>
2022-12-27 16:46:56 +0000

    Removed tech section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import { getSortedProjectsData } from "../api/getSortedProjectsData"
+import { getSortedProjectsData } from "../api/getSortedProjectsData";
 
-import Container from '../components/Container';
-import Hero from '../components/sections/hero';
-import About from '../components/sections/about';
-import Projects from '../components/sections/projects';
-import Contact from '../components/sections/contact';
-import Tech from '../components/sections/tech';
-import { Page } from '../components/page';
+import Container from "../components/Container";
+import Hero from "../components/sections/hero";
+import About from "../components/sections/about";
+import Projects from "../components/sections/projects";
+import Contact from "../components/sections/contact";
+import { Page } from "../components/page";
 
 const meta = {
-  title: 'Digital design and development agency'
-}
+  title: "Digital design and development agency",
+};
 
 export default function Home({ allProjectsData }) {
   return (
@@ -20,10 +19,9 @@ export default function Home({ allProjectsData }) {
         <About />
         <Projects allProjectsData={allProjectsData} />
         <Contact />
-        <Tech />
       </Container>
     </Page>
-  )
+  );
 }
 
 export async function getStaticProps() {
