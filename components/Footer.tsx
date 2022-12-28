87449e1c5114b7c4ed08d07b3f14af523be85407
Sam Merrick <samjmerrick@gmail.com>
2022-12-28 18:07:41 +0000

    Automated year copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,28 @@
 import Icon from "@mdi/react";
 import { mdiEmailOutline } from "@mdi/js";
 import Container from "./Container";
+import { useEffect } from "react";
 
 function Footer() {
+  useEffect(() => {
+    document.getElementById("year").innerHTML = new Date()
+      .getFullYear()
+      .toString();
+  });
+
   return (
     <footer className="py-10">
       <Container>
         <div className="flex flex-col items-center justify-between space-y-5 text-center md:flex-row">
           <p className="flex flex-row items-center space-x-3">
-            <Icon path={mdiEmailOutline} size={1.5} className="text-zinc-900" />
+            <Icon path={mdiEmailOutline} size={1} className="text-zinc-900" />
 
             <a href="mailto:hello@zero-1.studio">hello@zero-1.studio</a>
           </p>
           <div>
-            <p>© Zero 1 Studio 2022</p>
-            <a href="https://storyset.com/work">
-              Work illustrations by Storyset
-            </a>
+            <p>
+              © <span id="year"></span> Zero 1 Studio{" "}
+            </p>
           </div>
         </div>
       </Container>
