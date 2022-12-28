229ff4a2bda1099023cf7f2eb562e4d1d27c325b
Sam Merrick <samjmerrick@gmail.com>
2022-12-28 09:58:52 +0000

    Added buttons on hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -4,16 +4,30 @@ import Container from "../components/Container";
 
 function Hero() {
   return (
-    <div className=" relative bg-slate-200 sm:py-20 md:py-60">
+    <div className=" relative bg-slate-200 py-20 md:py-32 lg:py-40 xl:py-48 2xl:py-60">
       <Container>
         <div className="flex w-1/2 flex-col space-y-8">
           <h1 className="text-4xl md:text-5xl ">
             Digital experiences from Zero to One
           </h1>
-
-          <p className="text-2xl">
-            We help startups and scale-ups to build innovative digital products.
+          <p className="text-xl">
+            We help startups and scale-ups to build innovative digital products
+            that solve real user problems.
           </p>
+          <div className="flex flex-row space-x-3">
+            <a
+              className="cursor-pointer rounded-md bg-slate-600 py-3 px-5 text-lg font-bold text-white hover:bg-slate-700"
+              href="#Contact"
+            >
+              Get in touch
+            </a>
+            <a
+              className="cursor-pointer rounded-md border border-slate-800 py-3 px-5 text-lg font-bold hover:bg-slate-300"
+              href="#About"
+            >
+              Learn More
+            </a>
+          </div>
         </div>
       </Container>
 
