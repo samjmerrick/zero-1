3ef7df4217c08252aa80baae73afa0716a74db93
Sam Merrick <samjmerrick@gmail.com>
2022-12-29 14:27:19 +0000

    Removed tw-elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Script from 'next/script'
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-    useEffect(() => {
-        const use = async () => {
-            (await import('tw-elements')).default;
-        };
-        use();
-    }, []);
-
-
-    return <>
-        <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-DM4ZKX9JBJ"
-            strategy="lazyOnload"
-        />
-        <Script id="google-analytics" strategy="lazyOnload">
-            {`
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DM4ZKX9JBJ"
+        strategy="lazyOnload"
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){window.dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'G-DM4ZKX9JBJ');
             `}
-        </Script>
-        <Component {...pageProps} />
+      </Script>
+      <Component {...pageProps} />
     </>
+  );
 }
 
-export default MyApp
+export default MyApp;
