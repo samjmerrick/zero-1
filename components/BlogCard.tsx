63e4e578bcad72f3d684de4fef8cb4401bdf2f86
Sam Merrick <samjmerrick@gmail.com>
2023-02-05 16:32:26 +0000

    Hover on blog card

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { useState } from "react";
 import Image from "next/image";
 
 dayjs.extend(relativeTime);
@@ -11,17 +10,16 @@ export default function BlogCard(props: { matter: BlogFrontmatter }) {
     return dayjs().to(new Date(date));
   }
 
-  const [hover, setHover] = useState(false);
-
   const matter = props.matter;
 
   return (
-    <Link
-      href={`/blog/${matter.slug}`}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <div className=" rounded-md bg-neutral-50">
+    <Link href={`/blog/${matter.slug}`}>
+      <div
+        className=" rounded-md bg-neutral-50      shadow-md
+                transition-all
+                hover:-translate-y-0.5
+                hover:shadow-xl"
+      >
         <Image
           src={matter.image}
           className="rounded-t-md"
@@ -37,9 +35,6 @@ export default function BlogCard(props: { matter: BlogFrontmatter }) {
           <h3 className="py-2 text-2xl font-bold">{matter.title}</h3>
 
           <p className="pb-2">{matter.summary}</p>
-          <p className={`text-xs ${hover ? " text-blue-700" : null}`}>
-            <span className="underline">Read More</span> →
-          </p>
         </div>
       </div>
     </Link>
