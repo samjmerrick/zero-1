0460f59049ee2090495a1bf0c8b71b35fede27ef
Sam Merrick <samjmerrick@gmail.com>
2023-02-08 09:34:49 +0000

    Animated mobile menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,6 +7,7 @@ import { twMerge } from "tailwind-merge";
 
 import Container from "components/Container";
 import Logo from "components/Logo";
+import { motion } from "framer-motion";
 
 function Nav() {
   const navigation = [{ name: "Blog", href: "/blog" }];
@@ -22,24 +23,24 @@ function Nav() {
     >
       <Container>
         <div className="flex items-center justify-between">
-          <Link href="/">
+          <Link href="/" className="z-20">
             <Logo
               className={isHome ? "text-neutral-200" : "text-neutral-900"}
             />
           </Link>
 
-          <button id="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="z-20 md:hidden"
+          >
             <Icon
               path={!menuOpen ? mdiMenu : mdiClose}
-              className={twMerge(
-                "h-8 w-8 md:hidden",
-                isHome && "text-neutral-300"
-              )}
+              className={twMerge("h-8 w-8", isHome && "text-neutral-300")}
             />
           </button>
 
           <div
-            className="hidden flex-row items-center space-x-10 pt-6 pr-2 text-lg md:flex md:pt-0"
+            className="hidden flex-row items-center space-x-12 pt-6 pr-2 text-lg md:flex md:pt-0"
             id="menu"
           >
             {navigation.map((nav) => (
@@ -59,8 +60,8 @@ function Nav() {
               className={twMerge(
                 "cursor-pointer rounded-lg  py-2 px-5 text-lg font-semibold transition-colors ",
                 isHome
-                  ? "bg-neutral-800 text-white hover:bg-neutral-700"
-                  : "bg-neutral-200 text-neutral-700 hover:bg-neutral-300"
+                  ? "bg-neutral-200 text-neutral-800 hover:bg-neutral-400"
+                  : "bg-neutral-800 text-neutral-200 hover:bg-neutral-700"
               )}
               href="mailto:hello@zero-1.studio"
             >
@@ -69,25 +70,56 @@ function Nav() {
           </div>
         </div>
 
-        <div
-          id="mobile-menu"
+        <motion.div
           className={twMerge(
-            "absolute left-2 right-2 z-20 rounded-md bg-neutral-50 text-lg shadow-sm md:hidden",
-            !menuOpen && "hidden"
+            "absolute top-0 left-0 right-0 bottom-0 z-10 touch-none rounded-t-md  md:hidden",
+            isHome ? "bg-neutral-900" : "bg-neutral-100"
           )}
+          variants={{
+            open: {
+              scale: 1,
+              opacity: 1,
+              translateY: 0,
+            },
+            closed: {
+              scale: 0,
+              opacity: 0,
+              translateY: "-30%",
+              transition: {
+                type: "tween",
+                duration: 0.3,
+              },
+            },
+          }}
+          initial="closed"
+          animate={menuOpen ? "open" : "closed"}
         >
-          <div className="flex flex-col space-y-4 py-5 text-center">
+          <div className="flex h-full flex-col justify-center space-y-20">
             {navigation.map((nav) => (
               <Link
                 href={nav.href}
                 onClick={() => setMenuOpen(false)}
-                className="block text-center md:inline-block"
+                className={twMerge(
+                  "block text-center text-3xl font-semibold ",
+                  isHome ? "text-neutral-100" : "text-neutral-800"
+                )}
               >
                 {nav.name}
               </Link>
             ))}
+            <a
+              className={twMerge(
+                "mx-4 block rounded-md  py-6 text-center text-3xl font-semibold ",
+                isHome
+                  ? "bg-neutral-100 text-neutral-800"
+                  : "bg-neutral-800 text-neutral-200"
+              )}
+              href="mailto:hello@zero-1.studio"
+            >
+              Contact
+            </a>
           </div>
-        </div>
+        </motion.div>
       </Container>
     </nav>
   );
