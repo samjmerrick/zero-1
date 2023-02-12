7add581d0728c0f007be482c8108237b0c95963f
Sam Merrick <samjmerrick@gmail.com>
2023-02-12 16:40:18 +0000

    Add button component

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+import { twMerge } from "tailwind-merge";
+
+function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        "cursor-pointer rounded-lg  bg-neutral-800 py-2 px-5 text-lg font-semibold text-neutral-200 transition-colors hover:bg-neutral-700 ",
+        props.className
+      )}
+    />
+  );
+}
+
+export default Button;
