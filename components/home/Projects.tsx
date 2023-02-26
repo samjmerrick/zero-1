ff11009133f963bd711108c97cfb46a630c03d5c
Sam Merrick <samjmerrick@gmail.com>
2023-02-26 16:17:25 +0000

    Added contact CTA at end of projects

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,15 +1,34 @@
 import ImageCard from "components/layout/ImageCard";
 import Label from "components/Label";
-import { useState } from "react";
-import { twMerge } from "tailwind-merge";
+import Button from "components/Button";
+import Icon from "@mdi/react";
+import { mdiSend } from "@mdi/js";
 
 function Projects(props: { projects: ProjectFrontMatter[] }) {
   return (
     <section id="Projects" className=" py-20">
-      <div className="flex flex-col space-y-24">
+      <div className="flex flex-col items-center space-y-24">
         {props.projects.map((project, i) => (
           <Project project={project} position={i} />
         ))}
+        <div className="flex w-full max-w-2xl flex-col items-center justify-center space-y-6 rounded-lg border-2 border-dashed border-neutral-500 py-14 px-6 text-center">
+          <h2 className="text-3xl font-bold">Your Project Here</h2>
+          <div className="text-neutral-600">
+            <p className="pb-2">
+              We're excited to learn about your project, no matter how far along
+              you are!
+            </p>
+            <p>
+              Shoot us a message and let's see how we can bring your vision to
+              life.
+            </p>
+          </div>
+
+          <Button onClick={() => window.open("mailto:hello@zero-1.studio")}>
+            <Icon className="mr-4 inline-block w-6" path={mdiSend} />
+            Get in touch
+          </Button>
+        </div>
       </div>
     </section>
   );
