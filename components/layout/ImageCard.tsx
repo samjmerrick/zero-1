cbe9a3a672a4380bff3f40589c5ca4638c04b8a3
Sam Merrick <samjmerrick@gmail.com>
2023-02-28 20:10:35 +0000

    Removed parallax

--- components/layout/ImageCard.tsx
+++ components/layout/ImageCard.tsx
@@ -1,38 +1,31 @@
 import { StaticImageData } from "next/image";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
-import Parallax from "components/Parallax";
-import { useEffect } from "react";
-import useWindowSize from "components/useWindowSize";
 
 function ImageCard(props: {
   image: StaticImageData;
   children: React.ReactNode;
   cardLocation?: "Right" | "Left";
 }) {
-  const screenSize = useWindowSize();
-
   return (
     <div className="grid w-full grid-cols-1 items-center md:grid-cols-10 ">
-      <Parallax
-        offset={0}
+      <div
         className={twMerge(
           "md:col-span-6 md:col-start-1 md:row-span-full",
           props.cardLocation == "Left" && "md:col-start-5 md:col-end-11"
         )}
       >
         <Image src={props.image} width={1000} height={500} alt="" />
-      </Parallax>
+      </div>
 
-      <Parallax
-        offset={screenSize.width > 768 ? 20 : 0}
+      <div
         className={twMerge(
           "z-10 bg-white p-8 shadow-xl md:col-span-5 md:col-end-11 md:row-span-full md:p-12",
           props.cardLocation == "Left" && "md:col-start-1 md:col-end-6"
         )}
       >
         {props.children}
-      </Parallax>
+      </div>
     </div>
   );
 }
